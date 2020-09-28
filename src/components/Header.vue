90aaf9aa0373b773716a544cc1aa86582b3e6995
kstawinski <stawinskidev@gmail.com>
2020-09-28 09:34:08 +0200

    Create Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+    flat
+  >
+    <v-toolbar-title>Material Design Icons</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-menu
+      transition="slide-y-transition"
+      bottom
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Random icon</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Header',
+});
+</script>
