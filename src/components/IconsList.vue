7fa8fa510683e987770122dc1e29d5ccb1208098
kstawinski <stawinskidev@gmail.com>
2020-09-28 09:47:25 +0200

    Change code-style

--- src/components/IconsList.vue
+++ src/components/IconsList.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row class="ma-3">
-    <span v-for='(icon, i) in icons' :key='i'>
+    <span v-for="(icon, i) in icons" :key="i">
       <Icon :name="icon.name" />
     </span>
   </v-row>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Icon from '@/components/Icon.vue';
 import Vue from 'vue';
 
