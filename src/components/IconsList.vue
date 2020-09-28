265a2346a7ef3bdbce10f323aadf966ae75180fa
kstawinski <stawinskidev@gmail.com>
2020-09-28 10:02:54 +0200

    Add .json file with icons list

--- src/components/IconsList.vue
+++ src/components/IconsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row class="ma-3">
+  <v-row class="ma-3 d-flex">
     <span v-for="(icon, i) in icons" :key="i">
       <Icon :name="icon.name" />
     </span>
@@ -9,6 +9,7 @@
 <script lang="ts">
 import Icon from '@/components/Icon.vue';
 import Vue from 'vue';
+import iconsJSON from '../data/icons.json';
 
 export default Vue.extend({
   name: 'IconsList',
@@ -18,45 +19,7 @@ export default Vue.extend({
   },
 
   data: () => ({
-    icons: [
-      {
-        id: 'CBFA6722-0EE6-49B4-B5C2-0B177A5523C2',
-        name: 'ab-testing',
-        codepoint: 'F01C9',
-        aliases: [],
-        tags: [
-          'Developer Languages',
-        ],
-        author: 'Michael Richins',
-        version: '4.0.96',
-      },
-      {
-        id: 'E20665F3-5354-46C2-9162-4202B20DCA05',
-        name: 'abjad-arabic',
-        codepoint: 'F1328',
-        aliases: [
-          'writing-system-arabic',
-        ],
-        tags: [
-          'Alpha Numeric',
-        ],
-        author: 'Haley Halcyon',
-        version: '4.9.95',
-      },
-      {
-        id: 'FC508376-0233-498A-ABF8-B289241031FF',
-        name: 'abjad-hebrew',
-        codepoint: 'F1329',
-        aliases: [
-          'writing-system-hebrew',
-        ],
-        tags: [
-          'Alpha Numeric',
-        ],
-        author: 'Haley Halcyon',
-        version: '4.9.95',
-      },
-    ],
+    icons: iconsJSON.slice(0, 1000),
   }),
 });
 </script>
