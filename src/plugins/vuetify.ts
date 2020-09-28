118a28eca5ced13936c79b89c07e26f0cd40c0b4
kstawinski <stawinskidev@gmail.com>
2020-09-28 23:44:24 +0200

    Theme customization, update secondary color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#2196F3',
+        secondary: '#263238',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+      },
+    },
+  },
 });
