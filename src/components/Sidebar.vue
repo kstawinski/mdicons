5261f7ed9e603435ad621770bcc5b6e8c28b58f3
kstawinski <stawinskidev@gmail.com>
2020-09-30 22:52:27 +0200

    feat(search): emit search field value

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -8,14 +8,29 @@
       persistent-hint
       outlined
       clearable
+      v-model="search"
+      @keypress="emitValue"
     ></v-text-field>
   </v-row>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue';
+import eventBus from '../plugins/eventBus';
 
 export default Vue.extend({
   name: 'Sidebar',
+
+  data: () => ({
+    search: undefined,
+  }),
+
+  methods: {
+    emitValue() {
+      if (this.search.length >= 3) {
+        eventBus.$emit('search', this.search);
+      }
+    },
+  },
 });
 </script>
