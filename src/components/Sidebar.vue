89ffc3641aaec62967d78eca2bf8a32113b6afee
kstawinski <stawinskidev@gmail.com>
2020-09-30 22:39:01 +0200

    feat(sidebar): create search field

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,15 @@
 <template>
-  <p>sidebar</p>
+  <v-row>
+    <!-- Search -->
+    <v-text-field
+      label="Search"
+      prepend-inner-icon="mdi-magnify"
+      hint="Keywords for icon what you are looking for."
+      persistent-hint
+      outlined
+      clearable
+    ></v-text-field>
+  </v-row>
 </template>
 
 <script lang='ts'>
