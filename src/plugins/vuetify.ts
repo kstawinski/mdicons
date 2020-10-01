ad5d1847f71c2d005b4b628a439d6d7f86b866a2
Kacper Stawinski <stawinskidev@gmail.com>
2020-10-01 16:53:15 +0200

    fix: modify theme colors (closes #24)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,14 +6,7 @@ Vue.use(Vuetify);
 export default new Vuetify({
   theme: {
     themes: {
-      light: {
-        primary: '#5e21f3',
-        secondary: '#263238',
-        accent: '#8c9eff',
-        error: '#b71c1c',
-      },
       dark: {
-        primary: '#411b9e',
         secondary: '#FFFFFF',
       },
     },
