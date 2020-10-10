e271256bc7a3a7ad43e26f5b35885fc7fdcfa7a2
Kacper Stawinski <stawinskidev@gmail.com>
2020-10-10 15:35:01 +0200

    fix: remove eventBus declaration

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import VuePrism from '@aminoeditor/vue-prism';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import eventBus from './plugins/eventBus';
 
 Vue.config.productionTip = false;
 
 Vue.use(VuePrism);
-Vue.use(eventBus);
 
 new Vue({
   vuetify,
